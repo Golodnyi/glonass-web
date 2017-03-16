c1eadfb20885d2fb5176df3561176fd2905e0fa6
golodnyi <ochen@golodnyi.ru>
2017-03-16 13:48:53 +0700

    Динамическая навигация, с определение авторизации

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../models/User";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
-  public user: User;
+    public user: User;
 
-  constructor() { }
+    constructor(private authService: AuthService) {
+    }
 
-  ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem('user'));
-  }
+    ngOnInit() {
+        this.authService.isLoggedIn().subscribe(loggedIn => {
+            this.user = JSON.parse(localStorage.getItem('user'));
+        });
+    }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {User} from '../models/User';
 
-import {Observable} from 'rxjs/Rx';
+import {Observable, Subject} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {env} from "../../env";
@@ -11,6 +11,8 @@ import {Auth} from "../models/Auth";
 @Injectable()
 export class AuthService {
     private loginUrl = '/v1/auth/login';
+
+    private logger = new Subject<boolean>();
 
     constructor(private http: Http) {
     }
@@ -23,8 +25,19 @@ export class AuthService {
         return this.http.post(env.backend + this.loginUrl, 'email=' + auth.email + '&password=' + auth.password + '&remember=' + auth.remember, options)
             .map((response: Response) => {
                 localStorage.setItem('user', JSON.stringify(response.json()));
+                this.logger.next(true);
                 return response.json();
             })
             .catch((error: any) => Observable.throw(error.json().message || 'Server error'));
+    }
+
+    public logout()
+    {
+        localStorage.removeItem('user');
+        this.logger.next(false);
+    }
+
+    isLoggedIn(): Observable<boolean> {
+        return this.logger.asObservable();
     }
 }
