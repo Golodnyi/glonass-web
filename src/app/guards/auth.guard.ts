77454780f2ed553f2de0edb5cd6d26ec92d28350
golodnyi <ochen@golodnyi.ru>
2017-03-22 12:57:05 +0700

    Разгогиниваем через authGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import {CookieService} from 'angular2-cookie/core';
+import {AuthService} from "../services/auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router, private cookieService:CookieService) { }
+    constructor(private router: Router, private cookieService:CookieService, private authService: AuthService) { }
 
     canActivate() {
         if (localStorage.getItem('user')  && this.cookieService.get('token')) {
             return true;
         }
 
+        this.authService.logout();
         this.router.navigate(['/login']);
         return false;
     }
