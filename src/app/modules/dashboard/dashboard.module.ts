d7c2b5a0fd42f73bec6f922ed7524f91142ad480
golodnyi <ochen@golodnyi.ru>
2017-04-10 15:10:34 +0700

    cleanup

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,14 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {CompaniesComponent} from './companies/companies.component';
 import {CompaniesService} from "../../services/companies.service";
-import {SubdivisionsComponent} from "./subdivisions/subdivisions.component";
 import {AccordionModule} from 'ng2-bootstrap/accordion';
 import {TabsModule} from 'ng2-bootstrap/tabs';
 import {SubdivisionsService} from "../../services/subdivisions.service";
 import {CarsService} from "../../services/cars.service";
-import {CarsComponent} from "./cars/cars.component";
 import {NavigationComponent} from "./navigation/navigation.component";
 import {TreeModule} from 'primeng/primeng';
 
@@ -21,7 +18,7 @@ import {TreeModule} from 'primeng/primeng';
         TabsModule.forRoot(),
         TreeModule
     ],
-    declarations: [DashboardComponent, CompaniesComponent, SubdivisionsComponent, CarsComponent, NavigationComponent],
+    declarations: [DashboardComponent, NavigationComponent],
     bootstrap: [DashboardComponent],
     providers: [CompaniesService, SubdivisionsService, CarsService],
 })
