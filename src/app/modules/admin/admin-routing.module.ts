bd7d7857b59d0a9fe357846f482dc07dd54486c5
golodnyi <ochen@golodnyi.ru>
2017-04-13 15:09:48 +0700

    роутинг с сохранением навигации

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,18 +1,22 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AdminComponent} from './admin/admin.component';
 import {AdminGuard} from '../../guards/admin.guard';
 import {CompanyComponent} from './company/company.component';
 import {SubdivisionComponent} from './subdivision/subdivision.component';
 import {EngineComponent} from './engine/engine.component';
 import {CarComponent} from './car/car.component';
+import {AdminComponent} from './admin/admin.component';
 
 const routes: Routes = [
-    {path: 'admin', component: AdminComponent, canActivate: [AdminGuard]},
-    {path: 'admin/company/:id', component: CompanyComponent, canActivate: [AdminGuard]},
-    {path: 'admin/subdivision/:id', component: SubdivisionComponent, canActivate: [AdminGuard]},
-    {path: 'admin/car/:id', component: CarComponent, canActivate: [AdminGuard]},
-    {path: 'admin/engine/:id', component: EngineComponent, canActivate: [AdminGuard]},
+    {
+        path: 'admin', component: AdminComponent, canActivate: [AdminGuard],
+        children: [
+            {path: 'company/:id', component: CompanyComponent},
+            {path: 'subdivision/:id', component: SubdivisionComponent},
+            {path: 'car/:id', component: CarComponent},
+            {path: 'engine/:id', component: EngineComponent}
+        ]
+    }
 ];
 
 @NgModule({
