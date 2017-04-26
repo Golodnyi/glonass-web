8e3991117565ed9b8307d0a0c03313c0e59144b5
golodnyi <ochen@golodnyi.ru>
2017-04-26 15:35:55 +0700

    мелкие правки

--- src/app/modules/admin/companies/companies.module.ts
+++ src/app/modules/admin/companies/companies.module.ts
@@ -1,32 +1,31 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import {
+  AutoCompleteModule,
   ButtonModule,
   CalendarModule,
   DropdownModule,
   InputTextModule,
   TabMenuModule,
   ToolbarModule,
-  TreeModule,
-  AutoCompleteModule
+  TreeModule
 } from 'primeng/primeng';
-import {CompaniesRoutingModule} from './companies-routing.module';
-import {NavigationComponent} from './navigation/navigation.component';
-import {CarComponent} from './car/car.component';
-import {EngineComponent} from './engine/engine.component';
-import {CompaniesComponent} from './companies/companies.component';
-import {CompaniesService} from '../../../services/companies.service';
-import {SubdivisionsService} from '../../../services/subdivisions.service';
-import {CarsService} from '../../../services/cars.service';
-import {EnginesService} from '../../../services/engines.service';
-import {CompanyUpdateComponent} from './company/update/update.component';
-import {CompanyCreateComponent} from './company/create/create.component';
-import {SubdivisionCreateComponent} from './subdivision/create/create.component';
-import {SubdivisionUpdateComponent} from './subdivision/update/update.component';
-import {PipesModule} from '../../../pipes/pipes.module';
-import {TreePipe} from '../../../pipes/tree.pipe';
-import {DropDownPipe} from '../../../pipes/dropdown.pipe';
+import { CompaniesRoutingModule } from './companies-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CarComponent } from './car/car.component';
+import { EngineComponent } from './engine/engine.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { CompaniesService } from '../../../services/companies.service';
+import { SubdivisionsService } from '../../../services/subdivisions.service';
+import { CarsService } from '../../../services/cars.service';
+import { EnginesService } from '../../../services/engines.service';
+import { CompanyUpdateComponent } from './company/update/update.component';
+import { CompanyCreateComponent } from './company/create/create.component';
+import { SubdivisionCreateComponent } from './subdivision/create/create.component';
+import { SubdivisionUpdateComponent } from './subdivision/update/update.component';
+import { PipesModule } from '../../../pipes/pipes.module';
+import { TreePipe } from '../../../pipes/tree.pipe';
 
 @NgModule({
   imports: [
@@ -43,7 +42,10 @@ import {DropDownPipe} from '../../../pipes/dropdown.pipe';
     DropdownModule,
     AutoCompleteModule
   ],
-  declarations: [NavigationComponent, CompaniesComponent, CompanyUpdateComponent, CompanyCreateComponent, CarComponent, EngineComponent, SubdivisionCreateComponent, SubdivisionUpdateComponent],
+  declarations: [
+    NavigationComponent, CompaniesComponent, CompanyUpdateComponent, CompanyCreateComponent,
+    CarComponent, EngineComponent, SubdivisionCreateComponent, SubdivisionUpdateComponent
+  ],
   providers: [CompaniesService, SubdivisionsService, CarsService, EnginesService, TreePipe],
 })
 export class CompaniesModule {
