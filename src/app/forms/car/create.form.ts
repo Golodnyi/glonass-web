f9f22ccaee4267ec941243d69640612eddba9aa6
golodnyi <ochen@golodnyi.ru>
2017-05-10 10:34:01 +0700

    Реактивные формы для модели Car

---
+++ src/app/forms/car/create.form.ts
@@ -0,0 +1,22 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { Car } from '../../models/Car';
+
+@Injectable()
+export class CarCreateForm {
+  private form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  public create(car: Car) {
+    this.form = this.fb.group({
+      model_id: [car.model_id, Validators.required],
+      name: [car.name, Validators.required],
+      subdivision_id: [car.subdivision_id, Validators.required],
+      is_visible: [car.is_visible, Validators.required],
+    });
+
+    return this.form;
+  }
+}
