6c4c88323e30f2b909d07fb85487c4dcba8d1060
golodnyi <ochen@golodnyi.ru>
2017-05-18 22:43:55 +0700

    compiler error fix refactoring

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -8,40 +8,40 @@ import { Chart } from '../../models/Chart';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnChanges, OnDestroy {
-  private chart: any;
-  @Input() options: any;
+  public chart: any;
   @Input() data: any;
-  private i = 1000;
+
+  @Input() set options(options: any) {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    if (options) {
+      const currentChart = this.chart;
+      const config = new Chart(options, this.el.nativeElement.parentElement.offsetWidth);
+      config.xAxis.events = {
+        setExtremes: function (e) {
+          if (e.trigger !== 'syncExtremes') {
+            highstock.charts.forEach(function (chart) {
+              if (chart === undefined || chart === currentChart) {
+                return false;
+              }
+              chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {trigger: 'syncExtremes'});
+            });
+          }
+        }
+      };
+      this.chart = highstock.stockChart(this.el.nativeElement, config);
+    }
+  }
+
   ngOnChanges(changes: any) {
-    const options = changes.options;
     const data = changes.data;
     if (data) {
       if (!data.firstChange) {
         data.currentValue.forEach(point => {
-          point[0] += this.i;
           this.chart.series[0].addPoint(point, true);
         });
-        this.i += 1000;
-      }
-    }
-    if (options) {
-      if (options.firstChange) {
-        const config = new Chart(this.options, this.el.nativeElement.parentElement.offsetWidth);
-        config.xAxis.events = {
-          setExtremes: function (e) {
-            if (e.trigger !== 'syncExtremes') {
-              highstock.charts.forEach(function (chart) {
-                if (chart === undefined || chart === this.chart) {
-                  return false;
-                }
-                chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {trigger: 'syncExtremes'});
-              });
-            }
-          }
-        };
-        this.chart = highstock.stockChart(this.el.nativeElement, config);
-      } else {
-        this.options = options.currentValue;
       }
     }
   }
