cb1f017c2d0d81fa97795ed3bdadfb0705fb948a
golodnyi <ochen@golodnyi.ru>
2017-05-24 10:14:07 +0700

    Отписки в навигации пользователей админки

--- src/app/modules/admin/users/navigation/navigation.component.ts
+++ src/app/modules/admin/users/navigation/navigation.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MsgService } from '../../../../services/msg';
 import { UsersService } from '../../../../services/users.service';
 import { User } from '../../../../models/User';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnDestroy {
 
   public users: User[];
+  private us: Subscription;
 
   constructor(private msgService: MsgService, private usersService: UsersService, private router: Router) {
-  }
-
-  ngOnInit() {
-    this.usersService.all().subscribe(
+    this.us = this.usersService.all().subscribe(
       users => {
         this.users = users;
       },
@@ -25,6 +24,12 @@ export class NavigationComponent implements OnInit {
         this.msgService.notice(MsgService.ERROR, 'Ошибка', error);
       }
     );
+  }
+
+  ngOnDestroy() {
+    if (this.us) {
+      this.us.unsubscribe();
+    }
   }
 
   public onNodeSelect(event: any) {
