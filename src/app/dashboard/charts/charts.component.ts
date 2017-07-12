ff6cd70c91c79fa4c3818052510d4705f403dbdf
golodnyi <ochen@golodnyi.ru>
2017-07-12 09:54:51 +0700

    Рефакторинг

--- src/app/dashboard/charts/charts.component.ts
+++ src/app/dashboard/charts/charts.component.ts
@@ -36,10 +36,28 @@ export class ChartsComponent implements OnDestroy {
   public mapPolyLines: MapPolyLines[];
   public move = false;
 
-  constructor(private route: ActivatedRoute,
-              private chartsService: ChartsService,
-              private carsService: CarsService,
-              private enginesService: EnginesService) {
+  private mapUpd(car_id: number) {
+    this.chartsService.mapData(car_id).subscribe(data => {
+      this.mapCars = [];
+      this.mapPolyLines = [];
+
+      if (data.length) {
+        const car = new MapCar();
+        car.name = this.car.name;
+        car.point = [data[data.length - 1][1], data[data.length - 1][2]];
+        this.mapCars.push(car);
+        const polyLines = new MapPolyLines();
+        polyLines.name = 'Маршрут';
+        polyLines.color = '#000000';
+        data.forEach(d => {
+          polyLines.points.push([d[1], d[2]]);
+        });
+        this.mapPolyLines.push(polyLines);
+      }
+    });
+  }
+
+  private filterInit() {
     this.subscription.add(this.route.queryParams.subscribe(filter => {
         if (Object.keys(filter).length) {
           this.filter = new Filter(filter);
@@ -50,102 +68,109 @@ export class ChartsComponent implements OnDestroy {
         }
       })
     );
+  }
 
-    this.subscription.add(this.route.params.subscribe(params => {
+  private carUpd(car_id: number) {
+    this.subscription.add(
+      this.carsService.get(car_id, true).subscribe(
+        car => {
+          this.chartsService.setCar(car);
+          this.car = car;
+        }
+      )
+    );
+  }
+
+  private engineUpd(car_id: number) {
+    this.subscription.add(
+      this.enginesService.get(1, 1, car_id, true).subscribe(
+        engine => {
+          this.engine = engine;
+        }
+      )
+    );
+  }
+
+  private chartsUpd() {
+    /**
+     * подписка на изменение данных
+     */
+    this.subscription.add(
+      this.chartsService.get().subscribe(
+        data => {
+          if (!data.length) {
+            this.options = [];
+          }
+          data.forEach(item => {
+            let exist = false;
+            this.options.forEach(options => {
+              if (options.id === item.id) {
+                options.data = item.data;
+                exist = true;
+              }
+            });
+            if (!exist) {
+              this.options.push(item);
+            }
+          });
+          this.autoRefresh.afterTime = this.lastTime();
+          this.chartsService.setAutoRefresh(this.autoRefresh);
+          this.loading = false;
+        }
+      )
+    );
+    // end
+  }
+
+  private filterUpd(car_id: number) {
+    /**
+     * Подписка на фильтр
+     */
+    if (this.subscriptionFilter) {
+      this.subscriptionFilter.unsubscribe();
+    }
+    this.subscriptionFilter = this.chartsService.getFilter().subscribe(
+      (filter) => {
+        this.loading = true;
+        this.filter = filter;
+        this.options = [];
+        this.autoRefresh.enabled = false;
+        this.chartsService.setAutoRefresh(this.autoRefresh);
+        this.chartsService.resync(car_id);
+      }
+    );
+  }
+
+  private autoRefreshUpd() {
+    if (this.subscriptionAutoRefresh) {
+      this.subscriptionAutoRefresh.unsubscribe();
+    }
+    this.subscriptionAutoRefresh = this.chartsService.getAutoRefresh().subscribe(
+      autoRefresh => {
+        if (!autoRefresh.enabled && this.subscriptionTimer) {
+          this.subscriptionTimer.unsubscribe();
+        }
+      }
+    );
+  }
+
+  constructor(private route: ActivatedRoute,
+              private chartsService: ChartsService,
+              private carsService: CarsService,
+              private enginesService: EnginesService) {
+    this.filterInit();
+    this.subscription.add(
+      this.route.params.subscribe(params => {
         this.options = []; // уничтожаем графики
         const car_id = +params['car'];
-        this.subscription.add(
-          this.carsService.get(car_id, true).subscribe(
-            car => {
-              this.chartsService.setCar(car);
-              this.car = car;
-            }
-          )
-        );
-        this.subscription.add(
-          this.enginesService.get(1, 1, car_id, true).subscribe(
-            engine => {
-              this.engine = engine;
-            }
-          )
-        );
-        this.chartsService.mapData(car_id).subscribe(data => {
-          this.mapCars = [];
-          this.mapPolyLines = [];
 
-          if (data.length) {
-            const car = new MapCar();
-            car.name = this.car.name;
-            car.point = [data[data.length - 1][1], data[data.length - 1][2]];
-            this.mapCars.push(car);
-            const polyLines = new MapPolyLines();
-            polyLines.name = 'Маршрут';
-            polyLines.color = '#000000';
-            data.forEach(d => {
-              polyLines.points.push([d[1], d[2]]);
-            });
-            this.mapPolyLines.push(polyLines);
-          }
-        });
-
-        /**
-         * подписка на изменение данных
-         */
-        this.subscription.add(
-          this.chartsService.get().subscribe(
-            data => {
-              if (!data.length) {
-                this.options = [];
-              }
-              data.forEach(item => {
-                let exist = false;
-                this.options.forEach(options => {
-                  if (options.id === item.id) {
-                    options.data = item.data;
-                    exist = true;
-                  }
-                });
-                if (!exist) {
-                  this.options.push(item);
-                }
-              });
-              this.autoRefresh.afterTime = this.lastTime();
-              this.chartsService.setAutoRefresh(this.autoRefresh);
-              this.loading = false;
-            }
-          )
-        );
-        // end
-
-        /**
-         * Подписка на фильтр
-         */
-        if (this.subscriptionFilter) {
-          this.subscriptionFilter.unsubscribe();
-        }
-        this.subscriptionFilter = this.chartsService.getFilter().subscribe(
-          (filter) => {
-            this.loading = true;
-            this.filter = filter;
-            this.options = [];
-            this.autoRefresh.enabled = false;
-            this.chartsService.setAutoRefresh(this.autoRefresh);
-            this.chartsService.resync(car_id);
-          }
-        );
-
-        if (this.subscriptionAutoRefresh) {
-          this.subscriptionAutoRefresh.unsubscribe();
-        }
-        this.subscriptionAutoRefresh = this.chartsService.getAutoRefresh().subscribe(
-          autoRefresh => {
-            if (!autoRefresh.enabled && this.subscriptionTimer) {
-              this.subscriptionTimer.unsubscribe();
-            }
-          }
-        );
-      }
-      )
+        this.carUpd(car_id);
+        this.engineUpd(car_id);
+        this.mapUpd(car_id);
+        this.chartsUpd();
+        this.filterUpd(car_id);
+        this.autoRefreshUpd();
+      })
     );
   }
 
