c1aea487537c9b06e2cc06a599a8b3f3c5402c7c
golodnyi <ochen@golodnyi.ru>
2017-07-18 13:01:33 +0700

    GW-4 Сброс моточасов реактивная форма, всплывающее окно, сервис

---
+++ src/app/dashboard/state/shared/reset.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../../shared/services/auth.service';
+import { MsgService } from '../../../shared/services/msg';
+import { Error } from '../../../shared/models/error.model';
+
+@Injectable()
+export class ResetService {
+  private host: string = environment.host;
+
+  constructor(private http: Http,
+              private authService: AuthService,
+              private router: Router,
+              private msgService: MsgService) {
+  }
+
+  public reset(data: any): Observable<any> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    const options = new RequestOptions({headers: headers, withCredentials: true});
+    return this.http.post(
+      this.host + '/v1/engine-maintenances/',
+      'date=' + data['date'] + '&comment=' + data['comment'],
+      options)
+      .map((response: Response) => {
+        return response.json();
+      })
+      .catch((error: any) => {
+        Error.check(error, this.authService, this.router, this.msgService);
+        return Observable.throw(error.json().message || 'Server error');
+      });
+  }
+}
--- src/app/dashboard/state/state.component.ts
+++ src/app/dashboard/state/state.component.ts
@@ -1,19 +1,40 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { State } from './shared/state.model';
 import * as moment from 'moment';
+import { Calendar } from '../../shared/models/calendar.model';
+import { FormGroup } from '@angular/forms';
+import { ResetForm } from './shared/reset.form';
+import { ResetService } from './shared/reset.service';
+import { MsgService } from '../../shared/services/msg';
 
 @Component({
   selector: 'app-state',
   templateUrl: './state.component.html',
-  styleUrls: ['./state.component.css']
+  styleUrls: ['./state.component.css'],
+  providers: [ResetForm, ResetService]
 })
 export class StateComponent implements OnChanges {
   @Input() state: State;
   @Input() title: string;
   @Input() toggleable = true;
   @Input() compact = false;
+  public ru = new Calendar();
   private audio = new Audio();
-  constructor() {
+  public display = false;
+  public form: FormGroup;
+  public submit: boolean;
+  private resetData: any;
+  constructor(private resetForm: ResetForm, private resetService: ResetService, private msg: MsgService) {
+    this.form = this.resetForm.create();
+    this.form.valueChanges
+      .map((value) => {
+        value.date = moment(value.date).format('YYYY-MM-DD');
+        return value;
+      })
+      .subscribe((data) => {
+        this.submit = false;
+        this.resetData = data;
+      });
     this.audio.src = '/assets/signal.mp3';
     this.audio.load();
   }
@@ -26,5 +47,22 @@ export class StateComponent implements OnChanges {
 
   public online() {
     return (Number(moment().format('X')) - Number(this.state.timestamp) / 1000) < 3600;
+  }
+
+  public showDialog() {
+    this.display = true;
+  }
+
+  public onSubmit() {
+    this.submit = true;
+    this.resetService.reset(this.resetData).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        this.submit = false;
+        this.msg.notice(MsgService.ERROR, 'Ошибка', error);
+      }
+    );
   }
 }
