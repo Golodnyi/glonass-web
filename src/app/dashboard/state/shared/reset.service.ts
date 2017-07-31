ed5d73e76939403892df69368f5bef5bb135131a
golodnyi <ochen@golodnyi.ru>
2017-07-31 11:12:41 +0700

    Разлогиниваем юзера со стороны сервера, при получении 409 ошибки encodeURIComponent

--- src/app/dashboard/state/shared/reset.service.ts
+++ src/app/dashboard/state/shared/reset.service.ts
@@ -27,7 +27,7 @@ export class ResetService {
     const options = new RequestOptions({headers: headers, withCredentials: true});
     return this.http.post(
       this.host + '/v1/engine-maintenances/',
-      'engine_id=' + car.engine.id + '&date=' + data.date + '&comment=' + data.comment,
+      'engine_id=' + car.engine.id + '&date=' + encodeURIComponent(data.date) + '&comment=' + data.comment,
       options)
       .map((response: Response) => {
         return response.json();
