f9d130ac2a17581b1ae5d4a32f5a4e81159eb5a0
golodnyi <ochen@golodnyi.ru>
2017-11-23 09:52:45 +0700

    Автообновление токена

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -8,13 +8,16 @@ import {Auth} from '../../login/shared/models/auth.model';
 import {MsgService} from './msg';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {JwtHelper, tokenNotExpired} from 'angular2-jwt';
+import {JwtHelper} from 'angular2-jwt';
 import {UsersService} from './users.service';
 import {Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Injectable()
 export class AuthService {
   private host: string = environment.host;
+  private timer = Observable.timer(0, 600000);
+  private subscriptionRefreshToken: Subscription;
 
   constructor(private http: HttpClient,
               private usersService: UsersService,
@@ -45,12 +48,47 @@ export class AuthService {
             const user = Object.assign(new User(), payload.user);
             user.role = payload.role;
             localStorage.setItem('User', JSON.stringify(user));
+
+            if (this.subscriptionRefreshToken) {
+              this.subscriptionRefreshToken.unsubscribe();
+            }
+
+            this.subscriptionRefreshToken = this.timer.subscribe(
+              () => {
+                this.refreshToken().subscribe();
+              }
+            );
+
+
             this.router.navigate(['/dashboard']);
           },
           error => {
             this.msg.notice(MsgService.ERROR, 'Ошибка получения пользователя', error);
           }
         );
+      })
+      .catch((error: any) => {
+        return Observable.throw(error.statusText || 'Server error');
+      });
+  }
+
+  public refreshToken(): Observable<boolean> {
+    return this.http.post(this.host + '/v1/auth/refresh', {refreshToken: localStorage.getItem('Refresh')}).map(
+      (data: any) => {
+        const jwtHelper: JwtHelper = new JwtHelper();
+        const token: any = jwtHelper.decodeToken(data.accessToken);
+
+        if (!token) {
+          return false;
+        }
+
+        if (jwtHelper.isTokenExpired(data.accessToken)) {
+          return false;
+        }
+
+        localStorage.setItem('Authorization', data.accessToken);
+        localStorage.setItem('Refresh', data.refreshToken);
+        return true;
       })
       .catch((error: any) => {
         return Observable.throw(error.statusText || 'Server error');
