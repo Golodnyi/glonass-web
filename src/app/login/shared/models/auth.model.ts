33ff8f069f80acdb50d888d88a46cc39090ec2d2
golodnyi <ochen@golodnyi.ru>
2017-11-28 10:35:11 +0700

    refactoring

--- src/app/login/shared/models/auth.model.ts
+++ src/app/login/shared/models/auth.model.ts
@@ -1,11 +1,9 @@
 export interface IAuth {
   email: string;
   password: string;
-  remember: number;
 }
 
 export class Auth implements IAuth {
   public email: string;
   public password: string;
-  public remember: number;
 }
