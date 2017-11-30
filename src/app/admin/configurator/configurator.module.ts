e7737c1d2c55730a0d92797a118ba6aaa5d19f34
golodnyi <ochen@golodnyi.ru>
2017-11-30 10:31:22 +0700

    компоненты навигации и схемы

--- src/app/admin/configurator/configurator.module.ts
+++ src/app/admin/configurator/configurator.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ConfiguratorRoutingModule } from './configurator-routing.module';
 import { ConfiguratorComponent } from './configurator/configurator.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SchemeComponent } from './scheme/scheme.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ConfiguratorRoutingModule
   ],
-  declarations: [ConfiguratorComponent]
+  declarations: [ConfiguratorComponent, NavigationComponent, SchemeComponent]
 })
 export class ConfiguratorModule { }
