be30c55013133c355436523d57632f182a22395f
Golodnyi <ochen@golodnyi.ru>
2018-05-14 11:08:27 +0700

    fix

--- src/app/shared/models/error.model.ts
+++ src/app/shared/models/error.model.ts
@@ -1,14 +1,15 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {Router} from '@angular/router';
-import {MsgService} from '../services/msg';
+import { Router } from '@angular/router';
+import { MsgService } from '../services/msg';
 
 @Injectable()
 export class Error {
     static check(error: any, router: Router, msgService: MsgService) {
-        if (error.status !== 200 && error.status !== 304) {
-            msgService.notice(MsgService.ERROR, 'Ошибка', error.error.message);
+        if (error.status !== 200 && error.status !== 304 && error.status !== 0) {
+            console.log('error', error);
+            msgService.notice(MsgService.ERROR, error.status, error.error.message);
 
             if (error.status === 401) {
                 localStorage.clear();
