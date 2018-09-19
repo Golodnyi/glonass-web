41ce5fd435fdbea93fb9a925d109edcaf81b1bff
Golodnyi <ochen@golodnyi.ru>
2018-09-19 10:41:56 +0700

    lint fix error

--- src/app/admin/monitoring/state/state.component.ts
+++ src/app/admin/monitoring/state/state.component.ts
@@ -16,13 +16,13 @@ export class StateComponent {
         return (Number(moment().format('X')) - Number(timestamp) / 1000) < 3600;
     };
 
-    public noConnections(timestamp: number) {
-        return (Number(moment().format('X')) - Number(timestamp) / 1000) > 172800;
-    }
-
     public static lastDate(timestamp: number) {
         return moment.unix(timestamp / 1000).format('DD.MM.YYYY H:mm');
     };
+
+    public noConnections(timestamp: number) {
+        return (Number(moment().format('X')) - Number(timestamp) / 1000) > 172800;
+    }
 
     public online(timestamp: number) {
         return StateComponent.online(timestamp);
