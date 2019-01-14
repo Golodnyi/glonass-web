d6f0abab9aca432b0145bc0a6de06870b40154d9
Golodnyi <ochen@golodnyi.ru>
2019-01-14 09:32:33 +0700

    Разделение бекенд сервера и сервера авторизации

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     host      : 'https://api.getcheck.pro',
+    auth_host : 'https://api.getcheck.pro',
     lang      : 'https://api.getcheck.pro/lang/'
 };
