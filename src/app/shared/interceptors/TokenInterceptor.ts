c91a834f08f666eef7c59f818982348de5be1438
Golodnyi <ochen@golodnyi.ru>
2019-01-15 09:20:48 +0700

    disable logging update gitignore

--- src/app/shared/interceptors/TokenInterceptor.ts
+++ src/app/shared/interceptors/TokenInterceptor.ts
@@ -18,7 +18,6 @@ export class TokenInterceptor implements HttpInterceptor {
 
   private addAuthenticationToken(req: HttpRequest<any>) {
     const token = localStorage.getItem('Authorization') || '';
-    console.log('Call addAuthenticationToken, token: ' + token);
 
     this.refreshTokenSubject.next(token);
     return req.clone({
@@ -36,42 +35,34 @@ export class TokenInterceptor implements HttpInterceptor {
     const token = localStorage.getItem('Authorization');
 
     if (!token) {
-      console.log('empty token, continue without token');
       return next.handle(req);
     }
 
-    console.log('Run intercept, token: ' + token);
     const jwtHelper = new JwtHelperService();
 
     if (!jwtHelper.isTokenExpired(token)) {
-      console.log('Token not expired, continue work...');
       req = this.addAuthenticationToken(req);
       return next.handle(req);
     }
 
     if (this.refreshTokenInProgress) {
-      console.log('Token already in refresh progress');
       return this.refreshTokenSubject
         .filter(result => result !== null)
         .take(1)
         .switchMap((result: any) => {
-          console.log('Refresh complite, token: ' + result);
           return next.handle(this.addAuthenticationToken(req));
         });
     } else {
       this.refreshTokenInProgress = true;
       this.refreshTokenSubject.next(null);
-      console.log('Token expired, send refresh request');
       return this.authService
         .refreshToken()
         .switchMap(() => {
           this.refreshTokenInProgress = false;
-          console.log('Refresh compite with service');
           return next.handle(this.addAuthenticationToken(req));
         })
         .catch((err: any) => {
           this.refreshTokenInProgress = false;
-          console.log('Error refresh token from service ' + err);
           this.authService.logout();
           return Observable.throw(err);
         });
